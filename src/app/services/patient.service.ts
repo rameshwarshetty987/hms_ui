f056e4396ca223b27fe3dc4ea9bc993600f5dc69
unknown <rameshwarshetty987@gmail.com>
2023-02-17 17:19:58 +0530

    patient form created

--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -1,17 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Patient } from '../common/patient';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientService {
 
-  private baseUrl = "http://localhost:8080/api/patient";
+  private baseUrl = "http://localhost:9099/api/patient";
 
   constructor(private httpClient : HttpClient) { }
 
-  createPatient(patient){
+  createPatient(patient : Patient) : Observable<any>{
      //return an observable
-     return this.httpClient.post(this.baseUrl,patient);
+     return this.httpClient.post(`${this.baseUrl}`,patient);
   }
+
 }
